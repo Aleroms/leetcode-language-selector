a06f70a5d35f8ee04874bbb182285087a5c437b8
aleroms <santiagomorales333@yahoo.com>
2024-01-14 19:28:18 -0800

    installed primeVue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'primevue/resources/themes/lara-light-green/theme.css'
+import PrimeVue from 'primevue/config'
+
 import App from './App.vue'
 import router from './router'
 
@@ -8,5 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
